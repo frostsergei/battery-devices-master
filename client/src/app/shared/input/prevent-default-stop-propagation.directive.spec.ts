0a9ff2fae0d27bb38802b66f90201f47e14e811b
Кривоченко Дмитрий <dima.krivochenko@mail.ru>
2024-04-13 13:17:19 +0300

    fix server: remove unnecesarry files

--- client/src/app/shared/input/prevent-default-stop-propagation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PreventDefaultStopPropagationDirective } from './prevent-default-stop-propagation.directive';
-
-describe('PreventDefaultStopPropagationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PreventDefaultStopPropagationDirective();
-    expect(directive).toBeTruthy();
-  });
-});

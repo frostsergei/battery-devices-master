8d8adfa4d0d7381454a95bc47752fb0c5bba654d
Кривоченко Дмитрий <dima.krivochenko@mail.ru>
2024-04-30 22:52:42 +0300

    feat client: add precision validator

---
+++ client/src/app/shared/validators/precision.ts
@@ -0,0 +1,18 @@
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+
+export function precisionValidator(precision: number): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const regex = new RegExp(`^-?\\d+(\\.\\d{1,${precision}})?$`);
+    if (
+      control.value === null ||
+      control.value === '' ||
+      regex.test(control.value)
+    ) {
+      return null; // Return null if there is no error
+    } else {
+      return {
+        precisionError: `Number is not formatted correctly, expected no more than ${precision} decimal places.`,
+      };
+    }
+  };
+}

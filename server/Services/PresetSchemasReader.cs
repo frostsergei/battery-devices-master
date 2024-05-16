4d846c382960a4a20aed0fa1ef2ee958f9fe7c03
Allonario <markush2004@mail.ru>
2024-05-16 20:15:51 +0300

    feat server: add form configuration to parameters response

--- server/Services/PresetSchemasReader.cs
+++ server/Services/PresetSchemasReader.cs
@@ -4,28 +4,35 @@ namespace BatteryDevicesMaster.Server.Services;
 
 public class PresetSchemasReader
 {
-    public List<ParametersSchema> ReadFiles(string directoryPath)
+    public List<ParametersSchema>? ReadFiles(string directoryPath, string parameterFilename, string formFilename)
     {
         var filesData = new List<ParametersSchema>();
 
-        if (Directory.Exists(directoryPath))
+        foreach (var dir in Directory.GetDirectories(directoryPath))
         {
-            var files = Directory.GetFiles(directoryPath);
-
-            foreach (var file in files)
+            if (Directory.Exists(dir))
             {
-                var fileName = Path.GetFileName(file);
-                var fileData = File.ReadAllText(file);
-
+                var parameterFilePath = Path.Combine(dir, parameterFilename);
+                var formFilePath = Path.Combine(dir, formFilename);
+                
+                if (!System.IO.File.Exists(parameterFilePath) || !System.IO.File.Exists(formFilePath))
+                {
+                    throw new InvalidOperationException("One or both YAML files not found");
+                }
+                
+                var parameterYaml = System.IO.File.ReadAllText(parameterFilePath);
+                var formYaml = System.IO.File.ReadAllText(formFilePath);
+                
+                var combinedYaml = parameterYaml + "\n" + formYaml;
+                
                 filesData.Add(new ParametersSchema
                 {
-                    FileName = fileName,
-                    FileData = fileData
+                    FileName = new DirectoryInfo(dir).Name,
+                    FileData = combinedYaml
                 });
             }
-            return filesData;
         }
-
-        return null;
+    
+        return filesData.Count > 0 ? filesData : null;
     }
 }
